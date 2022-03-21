788bd6917d5d93e9e51c00028315dd87ad0da870
Rafael Bandoni <rafagbandoni@gmail.com>
2022-03-21 19:42:17 -0300

    adding challenges version

--- class3/mentalist/app.js
+++ class3/mentalist/app.js
@@ -1,8 +1,13 @@
+// desafio 1: adicionar numero máximo de tentativas
+
 var numeroSecreto = parseInt(Math.random() * 11);
+var tentativas = 0;
+var contador = 3;
 
 function Chutar() {
     var chute = parseInt(document.getElementById("valor").value);
     var elementoResultado = document.getElementById("resultado");
+    var elementoContador = document.getElementById("contador");
     console.log(chute);
     
     if (chute == numeroSecreto) {
@@ -11,5 +16,23 @@ function Chutar() {
         elementoResultado.innerHTML = "Você deve digitar um número entre 0 e 10";
     } else {
         elementoResultado.innerHTML = "Você errou!";
+        contador--;
+        tentativas++;
+        if (tentativas == 3) {
+            elementoResultado.innerHTML = "Suas tentativas esgotaram. O número secreto é " + numeroSecreto;
+        }
+        elementoContador.innerHTML = "Tentativas restantes: " + contador;
     }
+
+//desafio 2: falar se o numero secreto é maior ou menor
+    var elementoMaiorOuMenor = document.getElementById("maioroumenor");
+
+    if (chute > numeroSecreto) {
+        elementoMaiorOuMenor.innerHTML = "O número secreto é menor.";
+    } else if (chute < numeroSecreto) {
+        elementoMaiorOuMenor.innerHTML = "O número secreto é maior.";
+    } else {
+        elementoMaiorOuMenor.innerHTML = "";
+    }
+        
 }
