51eae022b4a2cf5157b7a75241d03d0c6234b35c
Rafael Bandoni <rafagbandoni@gmail.com>
2022-03-21 18:29:14 -0300

    adding v1 of list exercise

--- class4/learn-lists/app.js
+++ class4/learn-lists/app.js
@@ -1,5 +1,12 @@
-var filme1 = "Yesterday";
-var filme2 = "A Chegada";
-var filme3 = "Escola de Rock";
+// usando listas
+var listaFilmes = ["Yesterday", "A Chegada", "Escola de Rock"];
 
-document.write("<p></p>")
+listaFilmes.push("Harry Potter");
+
+listaFilmes.push("Senhor dos Aneis");
+
+// é possível usar todas as tags do html aqui
+// usando o for
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<p>" + listaFilmes[i] + "</p>");
+}
